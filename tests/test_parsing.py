8966909eac6266977214e1cf5a1d41089097fafc
Josselin <josselin@trailofbits.com>
2021-12-14 09:21:12 +0100

    Manually ignore mypy on test_parsing

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -28,7 +28,7 @@ TARGETS = [
 ]
 
 
-@pytest.mark.parametrize("target", TARGETS)
+@pytest.mark.parametrize("target", TARGETS)  # type: ignore
 def test_parsing(target: str) -> None:
     with open(target) as f:
         teal = parse_teal(f.read())
