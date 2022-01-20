3992dd5f4f60f9b17472947c3ff87363b94cb894
S3v3ru5 <vara10110@gmail.com>
2022-01-20 17:46:03 +0530

    Add documentation to tealer.teal modules

--- tealer/teal/parse_teal.py
+++ tealer/teal/parse_teal.py
@@ -1,3 +1,30 @@
+"""Parser for teal contracts.
+
+A TEAL contract is represented using Teal class in tealer. Teal class
+comes with methods to register, run analyzers(detectors) and
+printers on the parsed contract. Parsing a teal contract is
+creating the Control Flow Graph(CFG) of the teal contract,
+constructing Teal class instance with the CFG, subroutines and
+other related information.
+
+Additional to parsing the teal contracts, subroutines defined in
+the contract are identified if there are any, versions of instructions
+are verified against the contract version and also attempts to detect mode
+of the contract. All the information is stored in the Teal class
+representing the contract.
+
+Contracts are parsed in four passes:
+
+#. Parses instructions and adds sequential instruction links.
+#. Adds jump based instruction links.
+#. Constructs basic blocks and adds sequential basic block links.
+#. Adds jump based basic block links.
+
+The final result of parsing is the Control Flow Graph(CFG) of the
+contract represented by sequence of the basic blocks.
+
+"""
+
 import sys
 from typing import Optional, Dict, List
 
@@ -24,6 +51,27 @@ from tealer.teal.teal import Teal
 
 
 def _detect_contract_type(instructions: List[Instruction]) -> ContractType:
+    """Determine type of contract given it's instructions.
+
+    It isn't possible to determine how a contract might be used, whether
+    as an application or a signature in all cases. This function uses the
+    fact that there are certain instructions in teal that are only valid if used
+    in a certain kind of contract. So, this function looks for instructions
+    that are valid in only one type and return that as the type for the given
+    contract. if all instructions in the contract are valid for both types
+    of contracts, then this function returns ContractType.ANY as it isn't
+    sure how the contract might be used.
+
+    Args:
+        instructions: List of all the instructions present in the contract.
+
+    Returns:
+        Type of the contract indicated by ContractType enum symbol.
+        ``STATEFULL`` if there's a instruction only valid in applications,
+        ``STATELESS`` if there's a instruction only valid in signatures,
+        ``ANY`` if there isn't any such instruction.
+    """
+
     for ins in instructions:
         if ins.mode != ContractType.ANY:
             return ins.mode
@@ -31,6 +79,34 @@ def _detect_contract_type(instructions: List[Instruction]) -> ContractType:
 
 
 def create_bb(instructions: List[Instruction], all_bbs: List[BasicBlock]) -> None:
+    """Construct basic blocks and add basic block sequential links.
+
+    This function is the third pass of the teal parser. Instructions are
+    divided into basic blocks based on the following rules:
+
+    * A new basic block is created for each Label instruction.
+    * Instructions B, Err, Return, Callsub, Retsub are exit instructions
+        of a basic block. A new basic block is created for instructions
+        following them.
+    * A instruction with multiple next instructions is a exit instruction
+        of the basic block. A new basic block is created for instructions
+        following them.
+
+    This function also adds sequential links between the constructed basic
+    blocks. Two sequential basic blocks are linked if the exit instruction
+    of the first basic block is sequentially previous instruction to the
+    entry instruction of the second basic block.
+
+    This is a "in place" function, given arguments are modified with the
+    data this function is supposed to return.
+
+    Args:
+        instructions: List of parsed instruction objects.
+        all_bbs: List of BasicBlock objects representing the teal contract.
+            This is an "in place" argument, the basic blocks created in
+            the function are appended to this list in the order they are created.
+    """
+
     bb: Optional[BasicBlock] = BasicBlock()
     if bb:  # to make mypy happy
         all_bbs.append(bb)
@@ -66,6 +142,42 @@ def _first_pass(
     rets: Dict[str, List[Instruction]],
     instructions: List[Instruction],
 ) -> None:
+    """Parse instructions and add sequential instruction links.
+
+    This function is the first pass of the teal parser. Source code
+    lines are parsed into corresponding Instruction objects and non-jump
+    instructions are linked. Adding instructions links is linking two
+    instruction objects as previous and next. Non-jump instruction links
+    are links between instructions that are previous, next in terms of
+    execution flow as well as previous, next in terms of source code i.e
+    links that are directly evident from just those two instructions that
+    are continous in source code. Jump links are links between branch
+    instructions and their destination, callsub and their destination,
+    retsub and their return points, etc. This function adds non-jump or
+    sequential instruction links.
+
+    This is a "in place" function, given arguments are modified with the
+    data this function is supposed to return.
+
+    Args:
+        lines: List of source code lines of a teal contract.
+        labels: Dict map from teal label string to the parsed label instruction.
+            instance. This argument is an "in place" argument and will be
+            populated by the function while parsing.
+        rets: Dict map from teal label string of a subroutine to list of it's
+            return point instructions. subroutines are supported from teal v4 and
+            can be called with callsub instruction using the label of the subroutine.
+            The execution flow is passed from the subroutine to next instruction of
+            the callsub after executing the subroutine. This variable stores all those
+            return points(next instruction after callsub) for each subroutine label
+            in the contract.
+            This argument is an "in place" argument and will be populated by the
+            function while parsing.
+        instructions: List of parsed instruction objects.
+            This argument is also an "in place" argument and will be populated
+            by the function while parsing.
+    """
+
     # First pass over the intructions list: Add non-jump instruction links and collect meta-data
     idx = 0
     prev: Optional[Instruction] = None  # Flag: last instruction was an unconditional jump
@@ -119,6 +231,23 @@ def _second_pass(
     labels: Dict[str, Label],
     rets: Dict[str, List[Instruction]],
 ) -> None:
+    """Add jump or non-sequential instruction links.
+
+    This function is second pass of the teal parser. Adding jump links is
+    linking two instruction objects where execution flow is passed
+    from the first instruction to the second instruction because of
+    a jump. A jump is passing execution from one instruction to the
+    next that are not sequential. A execution jump might happen because
+    of branch, callsub, retsub instructions in TEAL. This function links
+    such pairs of instructions as previous, next.
+
+    Args:
+        instructions: List of parsed instruction objects.
+        labels: Dict map from teal label string to the parsed label instruction.
+        rets: Dict map from teal label string of a subroutine to list of it's
+            return point instructions.
+    """
+
     # Second pass over the instructions list: Add instruction links for jumps
     for ins in instructions:
 
@@ -165,6 +294,17 @@ def _second_pass(
 
 
 def _fourth_pass(instructions: List[Instruction]) -> None:
+    """Add jump or non-sequential basic block links.
+
+    This function is the fourth pass of the teal parser. Jump links
+    are added between two basic blocks if there is a jump link from
+    exit instruction of the first basic block to the entry instruction
+    of the second basic block.
+
+    Args:
+        instructions: List of parsed instruction objects.
+    """
+
     # Fourth pass over the instructiions list: Add jump-based basic block links
     for ins in instructions:
         # A branching instruction with more than one target (other than a retsub)
@@ -193,6 +333,15 @@ def _fourth_pass(instructions: List[Instruction]) -> None:
 
 
 def _add_basic_blocks_idx(bbs: List[BasicBlock]) -> List[BasicBlock]:
+    """Set index of basic blocks based on their entry instruction line number.
+
+    Basic Blocks are ordered in the increasing order based on the
+    line number of their entry instructions. Index starts from 0.
+
+    Args:
+        bbs: List of BasicBlock objects representing the teal contract.
+    """
+
     bbs = sorted(bbs, key=lambda x: x.entry_instr.line)
     for i, bb in enumerate(bbs):
         bb.idx = i
@@ -208,8 +357,8 @@ def _identify_subroutine_blocks(label: "Label") -> List["BasicBlock"]:
 
     Returns:
         List["BasicBlock"]: list of all basic blocks part of a subroutine.
-
     """
+
     if label.bb is None:
         return []
 
@@ -242,22 +391,24 @@ def _identify_subroutine_blocks(label: "Label") -> List["BasicBlock"]:
 
 
 def _verify_version(ins_list: List[Instruction], program_version: int) -> bool:
-    """verify that instructions and fields used in the contract are supported in the
-    Teal version specified by the contract.
+    """Verify contract instructions and fields are supported in given teal version.
 
-    This function doesn't raise an exception in case of error, only returns a boolean representing
-    the presence of it and prints related information to stderr.
+    verify that instructions and fields used in the contract are supported in the
+    Teal version specified by the contract. This function doesn't raise an exception
+    in case of error, only returns a boolean representing the presence of it and
+    prints related information to stderr.
 
     Args:
         ins_list (List[Instruction]): list of contract instructions.
-        program_version (int): Teal version of the contract, calculated using #pragma version
-        instruction if it is the first instruction or else default version 1.
+        program_version (int): Teal version to check against the instruction and
+            fields version.
+
     Returns:
         (bool): returns true if there is any error i.e if any of the instructions or fields are
         not supported in the contract version Or if the contract contains instructions that are
         specific to both Signature and Application Mode.
-
     """
+
     stateful_ins: List[Instruction] = []
     stateless_ins: List[Instruction] = []
     error = False
@@ -305,6 +456,29 @@ def _verify_version(ins_list: List[Instruction], program_version: int) -> bool:
 
 
 def parse_teal(source_code: str) -> Teal:
+    """Parse algorand smart contracts written in teal.
+
+    Parsing teal cotracts consists of four passes:
+
+    #. Parses instructions and adds sequential instruction links.
+    #. Adds jump based instruction links.
+    #. Constructs basic blocks and adds sequential basic block links.
+    #. Adds jump based basic block links.
+
+    This function also performs basic version checks on the instructions
+    and fields.
+
+    Teal object is created with the parsed instructions, basic blocks and
+    subroutines identified in the contract.
+
+    Args:
+        source_code: TEAL source code of the contract.
+
+    Returns:
+        Teal object representing the given contract created with the related
+        information.
+    """
+
     instructions: List[Instruction] = []  # Parsed instructions list
     labels: Dict[str, Label] = {}  # Global map of label names to label instructions
     rets: Dict[str, List[Instruction]] = {}  # Lists of return points corresponding to labels
