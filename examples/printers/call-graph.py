232c9df24ebc62f9d8ef8a9e7cc5ba909da593a0
Vara Prasad Bandaru <vara.bandaru@trailofbits.com>
2023-02-09 15:49:05 +0530

    Add examples for printers

---
+++ examples/printers/call-graph.py
@@ -0,0 +1,40 @@
+from pyteal import *
+
+@Subroutine(TealType.none)
+def my_subroutine():
+    return Approve()
+
+@Subroutine(TealType.none)
+def recursive_subroutine(n: Expr) -> Expr:
+    return If(
+        n == Int(4),
+        Return(),
+        recursive_subroutine(n + Int(1)),
+    )
+
+@Subroutine(TealType.none)
+def main_subroutine() -> Expr:
+    return Seq([my_subroutine(), recursive_subroutine(Int(0))])
+
+router = Router(
+    name="CallGraphExample",
+    bare_calls=BareCallActions(),
+)
+
+@router.method(no_op=CallConfig.CALL)
+def method_a() -> Expr:
+    return Seq([my_subroutine()])
+
+@router.method(no_op=CallConfig.CALL)
+def method_b() -> Expr:
+    return Seq([main_subroutine()])
+
+@router.method(no_op=CallConfig.CALL)
+def method_c() -> Expr:
+    return Approve()
+
+pragma(compiler_version="0.22.0")
+application_approval_program, _, _ = router.compile_program(version=7)
+
+if __name__ == "__main__":
+    print(application_approval_program)
